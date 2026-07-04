b72dba11d56ea01a2185eba6f33d2d47a8fb13c7
Ramesh Errabolu <ramesh@linux.ibm.com>
2026-07-04 18:43:44 -0500

    sd-device: support slots pseudo subsystem
    Add support for the "slots" pseudo subsystem, enabling
    sd-device to discover and enumerate devices under
    /sys/bus/*/slots/.
    
    This enables replay of slot uevents during coldplug
    and allows uevents generated by udevadm trigger to be
    processed by udev rules.
    
    Depends-on: https://lore.kernel.org/all/20260520221320.99788-1-ramesh@linux.ibm.com/
    
    Signed-off-by: Ramesh Errabolu <ramesh@linux.ibm.com>

--- src/libsystemd/sd-device/device-internal.h
+++ src/libsystemd/sd-device/device-internal.h
@@ -27,7 +27,7 @@ struct sd_device {
 
         /* basic kernel properties */
         char *subsystem;
-        char *driver_subsystem; /* For selected subsystems (e.g., "drivers", etc) */
+        char *driver_subsystem; /* For selected subsystems (e.g., "drivers", "slots", etc) */
         char *driver;
         char *devtype;
 
@@ -122,6 +122,7 @@ int device_set_devtype(sd_device *device, const char *devtype);
 int device_set_devnum(sd_device *device, const char *major, const char *minor);
 int device_set_subsystem(sd_device *device, const char *subsystem);
 int device_set_diskseq(sd_device *device, const char *str);
+int device_set_slots_subsystem(sd_device *device);
 int device_set_drivers_subsystem(sd_device *device);
 int device_set_pseudo_subsystem(sd_device *device, const char *subsystem);
 int device_set_driver(sd_device *device, const char *driver);
